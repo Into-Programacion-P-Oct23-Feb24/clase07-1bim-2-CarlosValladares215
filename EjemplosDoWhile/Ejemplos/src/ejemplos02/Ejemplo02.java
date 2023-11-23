b8c4aab31a8d4748c4450d53e1fc142a0039ea6c
Carlos Valladares <csvalladares2@utpl.edu.ec>
2023-11-23 15:50:43 -0500

    Taller 07

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -28,14 +28,18 @@ public class Ejemplo02 {
         do {
             System.out.println("Ingrese calificación");
             nota = entrada.nextDouble();
-            cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
+            cadenaFinal = String.format("%s%.2f\n", cadenaFinal,
+                    nota);
             entrada.nextLine(); // limpieza de buffer
 
             System.out.println("Ingrese (s) si desea salir del ciclo ");
             salida = entrada.nextLine();
 
-            
-            
+            if (salida.equals("s") || salida.equals("si")
+                    || salida.equals("yes") || salida.equals("y"))
+            {
+                bandera = false;
+            }
 
         } while (bandera); // (bandera==true)
 
